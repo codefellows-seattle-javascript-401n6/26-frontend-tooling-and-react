bd07ed03f3d95a33b4625a2e63b9c19cc85b3954
daniel <dpillay03@gmail.com>
2018-04-17 15:22:15 -0700

    config files complete.

---
+++ lab-daniel/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (<div>
+            <h1>My React App</h1>
+        </div>)
+    }
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<App/>, root);
