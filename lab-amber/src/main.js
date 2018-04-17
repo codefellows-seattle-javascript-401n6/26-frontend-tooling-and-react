254891d0aad015e18d9f0debf34447041bff52d3
amgranad <ambergkim@gmail.com>
2018-04-17 10:57:56 -0700

    cow and random phrase working

--- lab-amber/src/main.js
+++ lab-amber/src/main.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import cowsay from 'cowsay';
+import {say} from 'cowsay';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      heading: 'My React app',
+      initialCow: say({text: faker.hacker.phrase()})
     }
+    // this.randomCow = this.randomCow.bind(this);
   }
+  // randomCow() {
+  //   this.setState(state => {
+  //     let cowArt = cowsay.say({text: 'random comment'});
+  //     return {cow: cowArt};
+  //   });
+  // }
   render() {
-    return <div className="App">
-      <h1>{this.state.heading}</h1>
+    return <div>
+      <pre>{this.state.initialCow}</pre>
       </div>
   }
 };
