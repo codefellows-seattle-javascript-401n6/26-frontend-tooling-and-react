7b8d2e3df882908fbc87cbfd14fcc2a49364c48b
dambergn <dambergn@gmail.com>
2018-04-16 21:11:08 -0700

    click update complete

--- lab-nick/main.js
+++ lab-nick/main.js
@@ -3,17 +3,38 @@ console.log('loading file');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import css from './style.css';
 
-class MyCoolApp extends React.Component {
+class MyCoolApp extends React.Component { // MyCoolApp is the name of the app
   constructor(props){
     super(props);
     console.log('constructing');
+    this.state = {
+      name: 'My React App',
+      count: 0
+    }
+    // make sure to bind the update count function
+    // so the context of "this" is always this class.
+    this.updateCount = this.updateCount.bind(this);
+  }
+
+  updateCount() {
+    console.log('paragraph clicked');
+    this.setState(state => {
+      return {count: state.count + 1};
+    });
   }
 
   render() {
-    return (<div>
-      <h1>My Cool App</h1>
-      </div>)
+    return <div>
+      <h1>this.state.name</h1>
+      <p> 
+        This is a page created with react
+      </p>
+      <p onClick={this.updateCount}>
+        People have clicked on this paragraph {this.state.count} times.
+      </p>
+      </div>;
   }
 }
 
--- lab-nick/webpack.config.js
+++ lab-nick/webpack.config.js
@@ -4,15 +4,18 @@
 const path = require('path');
 
 const config = {
-  mode: 'development', //production
-  entry: './main.js',
+  mode: 'development', // production
+  entry: './main.js', // name of java script file
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './dist'), // puts dist in current working directory
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      {test: /\.js$/, loader: 'babel-loader'}
+      {test: /\.js$/, loader: 'babel-loader'},
+      {test: /\.css/, loader: [
+        'style-loader', 'css-loader'
+      ]},
     ]
   }
 };
