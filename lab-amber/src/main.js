0407d37e1f07261330c5951cf850e1fa784ec58d
amgranad <ambergkim@gmail.com>
2018-04-17 12:23:05 -0700

    can choose squirrel for cow

--- lab-amber/src/main.js
+++ lab-amber/src/main.js
@@ -2,28 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import {say} from 'cowsay';
+import { think, SQUIRREL } from 'cowsay';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       heading: 'Generate Cowsay Lorem',
-      content: say({text: faker.hacker.phrase()})
+      selectValue: '',
+      content: say({
+        cow: this.selectValue,
+        text: faker.hacker.phrase()
+      })
     }
     this.randomComment = this.randomComment.bind(this);
+    this.newCow = this.newCow.bind(this);
   }
 
   randomComment() {
     this.setState(state => {
       let newComment = faker.hacker.phrase();
-      console.log('new comment', newComment);
-      return {content: say({text: newComment})};
+      return {content: say({
+        cow: this.state.selectValue,
+        text: newComment
+      })};
     });
+  }
+
+  newCow(e) {
+    console.log('target value', e.target.value);
+    let newComment = faker.hacker.phrase();
+    if (this.state.selectValue = 'squirrel') {
+      this.state.selectValue = SQUIRREL;
+      return {content: think({
+        cow: SQUIRREL,
+        text: newComment
+      })};
+    }
   }
 
   render() {
     return <div>
       <h1>{this.state.heading}</h1>
+      <select value={this.state.selectValue} onChange={this.newCow}>
+        <option value="">Select Cow</option>
+        <option value="squirrel">Squirrel</option>
+      </select>
       <button onClick={this.randomComment}>Click Me</button>
       <pre>{this.state.content}</pre>
       </div>
