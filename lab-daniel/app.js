51d497338ef8dec0c86e8c5a7c9bb062b66cae0d
daniel <dpillay03@gmail.com>
2018-04-17 19:47:45 -0700

    cowsay returning new lorem on click

--- lab-daniel/app.js
+++ lab-daniel/app.js
@@ -8,19 +8,28 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            header: 'Cowsay!',
-            cow: say({text: faker.hacker.phrase()})
+            header: 'Generate Cowsay Lorem!',
+            content: say({text: faker.hacker.phrase()})
         }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => {
+            let returnCow = faker.hacker.phrase();
+            return {content: say({text: returnCow})};
+        });
     }
     
     render() { 
         console.log(say({ text: 'grazing in the browser' }));
         return (<div>
             <h1>{this.state.header}</h1>
-            <pre>{this.state.cow}</pre>
+            <button onClick={this.handleClick}>Click Me</button>
+            <pre>{this.state.content}</pre>
         </div>)
     }
-}
+};
 
 const root = document.getElementById('root');
 ReactDOM.render(<App/>, root);
