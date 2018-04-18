25b22655dde1253e9eb24bb2125a432927ab3085
daniel <dpillay03@gmail.com>
2018-04-17 20:06:27 -0700

    README updated

--- lab-daniel/webpack.config.js
+++ lab-daniel/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const config = {
     mode: "development",
-    entry: "./app.js",
+    entry: "./src/main.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: 'bundle.js'
