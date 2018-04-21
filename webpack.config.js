e59fcacd6ce19c9228dc82570993032c0d842a05
Caitlin <caitlinsweeneylmt@gmail.com>
2018-04-21 12:13:01 -0700

    setting up and starting cowsay react app

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const config = {
+  mode : 'development',
+  entry : './main.js'
+  output : {
+    path : path.resolve(__dirname, '.dist'),
+    filename : 'bundle.js'
+  },
+  module : {
+    rules :[
+      {test: /\.js$/, loader: 'babel-loader'},
+      {test: /\.css$/, loader : ['style-loader','css-loader'
+    ]}
+    ]
+  }
+};
+
+module.exports = config;
