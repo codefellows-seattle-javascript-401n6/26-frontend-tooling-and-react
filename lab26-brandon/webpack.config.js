9817a834f279fded4e27504ad662bcd50f964520
Brandon Buchholz <blue.buchholz@gmail.com>
2018-04-27 19:54:02 -0700

    debugging app

---
+++ lab26-brandon/webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const config = {
+  mode: 'development',
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: '/\.css/',
+        loader: [
+          'style-loader', 'css-loader'
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
