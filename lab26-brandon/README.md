70b389a673ddf954edeb479b933193975f36fdef
Brandon Buchholz <blue.buchholz@gmail.com>
2018-05-04 14:19:57 -0700

    finished with lab 26 ready to turn in

---
+++ lab26-brandon/README.md
@@ -0,0 +1,15 @@
+# Code Fellows 401 Lab Random Cowsay Lorem
+
+## How to use:
+ Click on the 'moo' to generate a random cowsay lorem ipsum.
+
+## Description:
+This project is an an example in building a small React App using the technologies listed below. 
+
+## Technologies Used:
+* Node.js
+* Babel
+* React
+* Webpack
+* Cowsay
+* Faker
--- lab26-brandon/src/main.jsx
+++ lab26-brandon/src/main.jsx
@@ -1,6 +1,9 @@
+
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './src/style/style.css';
+import './style/style.css';
 import faker from 'faker';
 import {say} from 'cowsay';
 
@@ -10,12 +13,21 @@ class App extends React.Component {
  this.state = {
      heading: 'Generate Cowsay Lorem',
      content : '',
- }
- }
-
+   }
+   this.saySomething = this.saySomething.bind(this);
+    }
+    saySomething() {
+        this.setState({
+            content: say({
+                text: faker.lorem.sentence()
+            })
+        })
+    }
  render() {
    return <div>
-      <h1>{this.state.name}</h1>
+      <h1>Cowsay Lorem Ipsum</h1>
+      <button onClick={this.saySomething}>MOO!</button>
+      <pre>{this.state.content}</pre>
    </div>;
  }
 };
