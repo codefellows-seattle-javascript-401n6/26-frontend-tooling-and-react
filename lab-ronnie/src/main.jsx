b06b2344c7c4142490b493dd8ddb0d8542e9fdc1
ronnie1734 <rlewwwis@yahoo.com>
2018-05-06 20:18:56 -0700

    completed assigment

--- lab-ronnie/src/main.jsx
+++ lab-ronnie/src/main.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as cowsay from 'cowsay';
+import {say} from 'cowsay';
+import faker from 'faker';
 //import './style.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      heading: 'my React apppppp',
+      content: say({
+        text: faker.lorem.sentence()
+      }),
+      heading: 'Generate Cowsay Lorem'
     }
+
+    this.newText = this.newText.bind(this);
+  }
+
+  newText() {
+    this.setState({
+      content: say({
+        text: faker.lorem.sentence()
+      })
+    })
   }
   render() {
     return <div className="App">
       <h1>{this.state.heading}</h1>
+      <h2><button onClick={this.newText}>click me</button></h2>
+      <pre>{this.state.content}</pre>
     </div>
   }
 };
